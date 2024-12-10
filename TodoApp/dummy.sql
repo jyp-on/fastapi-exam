4201ff197926c2de9bcf1e5d65c6230966d66335
jyp-on <okmlnsunok@naver.com>
2024-12-10 22:44:41 +0900

    sqlite3 연결 및 데이터 조회

---
+++ TodoApp/dummy.sql
@@ -0,0 +1,11 @@
+INSERT INTO todos (title, description, priority, complete) VALUES
+('Buy groceries', 'Need to buy milk, bread, and eggs', 2, False),
+('Pay bills', 'Pay electricity and internet bills', 1, False),
+('Clean the house', 'Focus on the living room and kitchen', 3, True),
+('Read a book', 'Finish reading the current novel', 4, False),
+('Go for a walk', 'Take a 30-minute walk in the park', 5, True),
+('Call mom', 'Catch up and ask about her health', 2, False),
+('Fix the sink', 'The kitchen sink is leaking', 1, False),
+('Plan vacation', 'Research destinations and book flights', 4, False),
+('Organize closet', 'Sort clothes and donate old items', 3, True),
+('Workout', 'Complete a 1-hour gym session', 2, False);
---
+++ TodoApp/main.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+import models
+from models import Todos
+from database import engine, SessionLocal
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+  return db.query(Todos).all()
