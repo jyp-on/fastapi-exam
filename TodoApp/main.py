49c9f68fa00ce0f5aec69fac001ecf9acec5fbf1
jyp-on <okmlnsunok@naver.com>
2024-12-11 15:41:18 +0900

    단건조회, filter, first

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 from sqlalchemy.orm import Session
+from starlette import status
 
 import models
 from models import Todos
@@ -18,6 +19,15 @@ def get_db():
   finally:
     db.close()
 
-@app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
   return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+  todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+  if todo_model is not None:
+    return todo_model
+  raise HTTPException(status_code=404, detail='Todo not found.')
