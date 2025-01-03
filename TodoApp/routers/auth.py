e4d49f89eda6a85b4412643ea99a1f79604d5e7b
jyp-on <okmlnsunok@naver.com>
2025-01-03 21:55:53 +0900

    Authenticate Request: Post Todo

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from datetime import datetime, timedelta
-from fastapi import APIRouter, Depends
+
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
@@ -8,14 +9,19 @@ from starlette import status
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
-router = APIRouter()
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError, ExpiredSignatureError
+
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth'],
+)
 
 SECRET_KEY = 'f8c802d3312647686c20ecd709cd0a692f780d2976bf1ac8a5d70c03c3ff1852'
 ALGORITHMS = 'HS256'
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='/auth/token')
 
 def get_db():
   db = SessionLocal()
@@ -48,7 +54,31 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
   encode.update({'exp': expires})
   return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHMS)
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+  try:
+    payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHMS)
+    username: str = payload.get('sub')
+    user_id: int = payload.get('id')
+    if username is None or user_id is None:
+      raise HTTPException(
+          status_code=status.HTTP_401_UNAUTHORIZED,
+          detail='Could not validate credentials (missing fields)'
+      )
+    return {'username': username, 'id': user_id}
+  except ExpiredSignatureError:
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Token has expired'
+    )
+  except JWTError as e:
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f'Could not validate credentials ({str(e)})'
+    )
+
+
+@router.post("", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
                       create_user_request: CreateUserRequest):
   create_user_model = Users(
@@ -71,7 +101,8 @@ async def login_for_access_token(
 
   user = authenticate_user(form_data.username, form_data.password, db)
   if not user:
-    return 'Failed Authentication'
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail='Could not validate credentials')
 
   token = create_access_token(user.username, user.id, timedelta(minutes=20))
-  return token
+  return {"access_token": token, "token_type": "bearer"}
--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from starlette import status
 from models import Todos
 from database import SessionLocal
+from .auth import get_current_user
 
 router = APIRouter()
 
@@ -16,6 +17,7 @@ def get_db():
     db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 class TodoRequest(BaseModel):
   title: str = Field(min_length=3)
@@ -34,9 +36,16 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     return todo_model
   raise HTTPException(status_code=404, detail='Todo not found.')
 
-@router.post("/todo", status_code=status.HTTP_200_OK)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-  todo_model = Todos(**todo_request.model_dump())
+@router.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(user: user_dependency,
+                      db: db_dependency,
+                      todo_request: TodoRequest):
+
+  print(user)
+  if user is None:
+    raise HTTPException(status_code=401, detail='Authentication Failed')
+
+  todo_model = Todos(**todo_request.model_dump(), owner_id=user.get('id'))
 
   db.add(todo_model)
   db.commit()
